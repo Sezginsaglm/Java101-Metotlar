b2b7230c97ca58b58f7e145664e9184a0971786b
Sezginsaglm <114063403+Sezginsaglm@users.noreply.github.com>
2022-10-04 14:51:10 +0300

    Java101-Metotlar

---
+++ PalindromSayilar/src/PalindromSayilar.java
@@ -0,0 +1,19 @@
+public class PalindromSayilar {
+    static boolean isPalindrom(int number){
+        int temp=number,reverseNum=0,lastNum;
+        while (temp!=0){
+            lastNum=temp%10;
+            reverseNum=(reverseNum*10)+lastNum;
+            temp/=10;
+        }
+
+        if (number==reverseNum){
+            return true;
+        }else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println( isPalindrom(2442));
+    }
+}
